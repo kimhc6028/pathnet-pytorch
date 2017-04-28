d2fde16ae968154f1ec42c7df35b85b4c40289f2
Heecheol Kim <kimhc6028@gmail.com>
2017-04-28 11:51:35 +0900

    [modified] repeat script error fixed

--- repeat_experiment.sh
+++ repeat_experiment.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-for i in {0..100}
+for i in {0..20}
+do
+python main.py --no-graph --cifar-svhn --cifar-first
+done
+
+for i in {0..20}
 do
 python main.py --no-graph --cifar-svhn
 done
